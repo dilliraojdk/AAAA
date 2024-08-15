fd7b13df91409e4312f8229f23c76708cd17dacc
TL <TL@gmail.com>
2024-08-15 15:59:42 +0530

    SBExamples

---
+++ DemoExamplesApplication.java
@@ -0,0 +1,15 @@
+package com.app.pojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoExamplesApplication.class, args);
+		System.out.println("hiii");
+                 int i=20000;
+	}
+
+}
